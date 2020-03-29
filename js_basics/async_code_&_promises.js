bd8de954d4ed33a58a104b07230ad53f70f39b21
Imnmania <niloyb@live.com>
2020-03-29 23:21:11 +0600

    FUCK JAVASCRIPT

---
+++ js_basics/async_code_&_promises.js
@@ -0,0 +1,15 @@
+const fetchData = callback =>{
+    setTimeout(() => {
+        callback('Donee!');
+    }, 1500);
+};
+
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData(text => {
+        console.log(text);
+    });
+}, 2000);
+
+console.log('Hello');
+console.log('Gello');
